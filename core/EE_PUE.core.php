bdf1fc3cd3a69c2bd91dc8bfaa1a6e98c8b7f581
sethshoultes <sethshoultes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-17 00:12:18 +0000

    Changing to download updates from wordpress
    git-svn-id: https://plugins.svn.wordpress.org/event-espresso-decaf/trunk@991522 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/EE_PUE.core.php
+++ core/EE_PUE.core.php
@@ -179,7 +179,7 @@ class EE_PUE {
 				'option_key' => 'site_license_key', //this is what is used to reference the api_key in your plugin options.  PUE uses this to trigger updating your information message whenever this option_key is modified.
 				'options_page_slug' => 'espresso_general_settings',
 				'plugin_basename' => EE_PLUGIN_BASENAME,
-				'use_wp_update' => FALSE, //if TRUE then you want FREE versions of the plugin to be updated from WP
+				'use_wp_update' => TRUE, //if TRUE then you want FREE versions of the plugin to be updated from WP
 				'extra_stats' => $extra_stats
 			);
 			$check_for_updates = new PluginUpdateEngineChecker($host_server_url, $plugin_slug, $options); //initiate the class and start the plugin update engine!
